fa4aede5b04c6890c655321c2c136467b61db900
Tim Poulsen <tim@skypanther.com>
2013-08-20 12:56:54 -0400

    updated maps demo that uses <Module> tag

--- labcode/Demo/MapsDemo/app/controllers/index.js
+++ labcode/Demo/MapsDemo/app/controllers/index.js
@@ -1,54 +1,23 @@
 if(OS_ANDROID) {
-	var MapModule = require('ti.map');
-	var map1 = MapModule.createView({
-	    userLocation: true,
-	    mapType: MapModule.NORMAL_TYPE,
-	    animate: true,
-	    region: {latitude: -33.87365, longitude: 151.20689, latitudeDelta: 0.1, longitudeDelta: 0.1 },
-	    height: '50%',
-	    top: 0,
-	    left: 0,
-	    width: '50%'
-	});
-	
-	var map2 = MapModule.createView({
-	    userLocation: true,
-	    mapType: MapModule.TERRAIN_TYPE,
-	    animate: true,
-	    region: {latitude: -33.87365, longitude: 151.20689, latitudeDelta: 0.1, longitudeDelta: 0.1 },
-	    height: '50%',
-	    top: 0,
-	    right: 0,
-	    width: '50%'
-	});
-	
-	var map3 = MapModule.createView({
-	    userLocation: true,
-	    mapType: MapModule.SATELLITE_TYPE,
-	    animate: true,
-	    region: {latitude: -33.87365, longitude: 151.20689, latitudeDelta: 0.1, longitudeDelta: 0.1 },
-	    height: '50%',
-	    bottom: 0,
-		left: 0,
-	    width: '50%'
-	});
-	
-	var map4 = MapModule.createView({
-	    userLocation: true,
-	    mapType: MapModule.HYBRID_TYPE,
-	    animate: true,
-	    region: {latitude: -33.87365, longitude: 151.20689, latitudeDelta: 0.1, longitudeDelta: 0.1 },
-	    height: '50%',
-	    bottom: 0,
-	    right: 0,
-	    width: '50%',
-	    traffic: true
-	});
-	
-	$.win.add(map1);
-	$.win.add(map2);
-	$.win.add(map3);
-	$.win.add(map4);
+    // we need a reference to the ti.map module even though we're adding it via the XML
+    var MapModule = require('ti.map');
+
+    // for now, must add annotations in code
+    var anno1 = MapModule.createAnnotation({
+        title:"Mountain View", 
+        latitude:37.389569, 
+        longitude:-122.050212
+    });
+    $.map.addAnnotation(anno1);
+    var anno2 = MapModule.createAnnotation({
+        title:"Sydney", 
+        latitude: -33.87365, 
+        longitude: 151.20689
+    });
+    $.map1.addAnnotation(anno2);
+    
+    // have to set properties that rely on module constants in the controller
+    $.map1.mapType = MapModule.TERRAIN_TYPE;
 }
 
 
